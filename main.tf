7943b349c9a9f0b8c2615776275127aeee0f78f3
Lucas Bonanni <lbonanni@eventbrite.com>
2022-10-17 16:17:49 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-1"
+  profile = "recipe-app"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-026b57f3c383c2eec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+
+}
